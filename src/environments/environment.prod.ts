15726d2d7b8cbeb08bf80c2cb428bf906049cd0e
Jari Näser <jari.naeser@samtrevano.ch>
2020-11-07 17:23:05 +0100

    Add little updates to build app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiLink: "https://api.spacexdata.com/v4/",
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiamFyaS1uYWVzZXIyNCIsImEiOiJja2d3MzN6OGkwNjlyMzBvZW5tYjRhbnN2In0.dFCbN3gopy4LyIyASBG98Q'
+  }
 };
