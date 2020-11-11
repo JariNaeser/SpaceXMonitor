2112c6bcfd49210793c458c17b197b2873c414fd
Jari Näser <jari.naeser@samtrevano.ch>
2020-11-11 14:58:56 +0100

    Update countdown css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -33,31 +33,40 @@ export class HomeComponent implements OnInit {
 
   startCountdown(date){
     if(date != null){
-      //Countdown
-      var countDownDate = new Date(date).getTime();
+      (function () {
+        const second = 1000,
+              minute = second * 60,
+              hour = minute * 60,
+              day = hour * 24;
+      
+        let birthday = "Sep 30, 2021 00:00:00",
+            countDown = new Date(date).getTime(),
+            x = setInterval(function() {    
+      
+              let now = new Date().getTime(),
+                  distance = countDown - now;
+      
+                document.getElementById("days").innerText = Math.floor(distance / (day)) + "",
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)) + "",
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)) + "",
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second) + "";
+      
+              //do something later when date is reached
+              if (distance < 0) {
+                let headline = document.getElementById("headline"),
+                    countdown = document.getElementById("countdown"),
+                    content = document.getElementById("content");
+      
+                headline.innerText = "It's my birthday!";
+                countdown.style.display = "none";
+                content.style.display = "block";
+      
+                clearInterval(x);
+              }
+              //seconds
+            }, 0)
+        }());
 
-      var x = setInterval(function() {
-          // Get today's date and time
-          var now = new Date().getTime();
-
-          // Find the distance between now and the count down date
-          var distance = countDownDate - now;
-
-          // Time calculations for days, hours, minutes and seconds
-          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-          // Display the result in the element with id="demo"
-          $('#countdown').text(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
-
-          // If the count down is finished, write some text
-          if (distance < 0) {
-              clearInterval(x);
-              $('#countdown').text("EXPIRED");
-          }
-      }, 1000);
     }else{
       $('#countdown').text("Next event not found.");
     }  
