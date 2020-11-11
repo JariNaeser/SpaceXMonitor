e156a4817889f5dd417d7ae22d780273a6e2125d
Jari Näser <jari.naeser@samtrevano.ch>
2020-11-11 15:06:06 +0100

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -45,22 +45,20 @@ export class HomeComponent implements OnInit {
       
               let now = new Date().getTime(),
                   distance = countDown - now;
-      
-                document.getElementById("days").innerText = Math.floor(distance / (day)) + "",
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)) + "",
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)) + "",
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second) + "";
+
+              $('#days').text(Math.floor(distance / (day)));
+              $('#hours').text(Math.floor((distance % (day)) / (hour)));
+              $('#minutes').text(Math.floor((distance % (hour)) / (minute)));
+              $('#seconds').text(Math.floor((distance % (minute)) / second));
       
               //do something later when date is reached
               if (distance < 0) {
-                let headline = document.getElementById("headline"),
-                    countdown = document.getElementById("countdown"),
-                    content = document.getElementById("content");
-      
-                headline.innerText = "It's my birthday!";
-                countdown.style.display = "none";
-                content.style.display = "block";
-      
+                $('#link').css('display', 'none');
+                $('#days').text('-');
+                $('#hours').text('-');
+                $('#minutes').text('-');
+                $('#seconds').text('-');
+                
                 clearInterval(x);
               }
               //seconds
